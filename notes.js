79897b3c71089afd5535c113bef3022821123d86
MorentDennis <dennis.morent@student.howest.be>
2018-02-16 15:30:07 +0100

    adding notes

--- notes.js
+++ notes.js
@@ -1,5 +1,7 @@
 console.log('starting notes.js');
 
+const fs =  require('fs');
+
 
 
 module.exports.age = 25;
@@ -7,9 +9,43 @@ module.exports.add = (a, b) => {
     return a + b
 }
 
-const addNote = (title, body) => {
-    console.log('adding note')
+const fetchNotes = () => {
+    try {
+        return JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch (error) {
+        return [];
+    }
 }
+
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+} 
+
+
+
+const isDuplicatedNote  = (toCheckNote) =>
+{
+    let duplicateNotes = fetchNotes().filter(note => note.title == toCheckNote.title);
+    return (duplicateNotes.length > 0) 
+}
+
+
+const addNote = (title, body) => {
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    }
+    
+    if(!isDuplicatedNote(note))
+    {
+        notes.push(note);
+        saveNotes(notes);
+    }
+}
+
+
+
 
 const getAll = () => {
     
